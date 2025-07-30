1d9895734ad802862d5dbfe5a6528bd14f180384
Yasin Karasu <yasin.karasu5734@gmail.com>
2025-07-30 18:40:51 +0300

    4.useState

---
+++ 4.useState/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  // String tckn = "12345678901"
+  // useState ile tckn'yi tutalım : hooks
+
+  const[firstName, setFirstName] = useState("Yasin");
+  return (
+   <div>
+      <div>
+
+      </div>
+   </div>
+  )
+}
+
+export default App
