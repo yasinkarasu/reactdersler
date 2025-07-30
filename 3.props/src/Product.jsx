310b1e580f2b9274ed25042a2940a4a92342eb4c
Yasin Karasu <yasin.karasu5734@gmail.com>
2025-07-30 17:17:12 +0300

    Product

---
+++ 3.props/src/Product.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+function Product() {
+  return (
+    <div>
+        <div> ÜRÜN BİLGİLERİ </div> <hr />
+        <br />
+        <div>
+            <div> İsim : Ayakkabı </div>
+            <div>Fiyat : 4000 TL </div>
+            <div> Renk : Siyah </div>
+        </div>
+        <hr />
+        <div>
+            <div> İsim : Pantolon </div>
+            <div>Fiyat : 2000 TL </div>
+            <div> Renk : Mavi </div>
+        </div>
+ 
+    </div>
+  )
+}
+
+export default Product
