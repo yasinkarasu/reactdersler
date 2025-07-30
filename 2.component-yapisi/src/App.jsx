0b5dd42bf41d2c0cb7a55075991b89b60076f14f
Yasin Karasu <yasin.karasu5734@gmail.com>
2025-07-30 17:09:53 +0300

    3.props

--- 2.component-yapisi/src/App.jsx
+++ 2.component-yapisi/src/App.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import './App.css';
 import Login from './login';
 import {users} from './login';
+import Hello from './Hello';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      {/* <Login /> */}
+      <Hello />
     </div>
   );
 }
