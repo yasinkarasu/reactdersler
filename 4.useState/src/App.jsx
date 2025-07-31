14f835d2894d7944b24f59077e32fa433e37c0c2
Yasin Karasu <yasin.karasu5734@gmail.com>
2025-07-31 11:16:25 +0300

    count değeleri-vb

--- 4.useState/src/App.jsx
+++ 4.useState/src/App.jsx
@@ -8,12 +8,60 @@ function App() {
   // String tckn = "12345678901"
   // useState ile tckn'yi tutalım : hooks
 
-  const[firstName, setFirstName] = useState("Yasin");
-  return (
-   <div>
-      <div>
+  // useState : Bir state'in değerini set metodunu kullanarak değiştirdiğinde component yeniden render edilir.
 
-      </div>
+/*
+  const[firstName, setFirstName] = useState("Yasin");
+  const [names, setNames] = useState(["Yasin", "Şeyma"]);
+  const [userInfo, setUserInfo] = useState({ name: "Yasin", password: "123456" });
+  const [show, setShow] = useState(true);
+
+*/
+
+  const [count, setCount] = useState(0);
+
+
+  const arttir = () => {
+    setCount(count + 1);
+  }
+  const azalt = () => {
+    if (count <= 0) {
+      return;
+    }
+    // count = cousnt - 1; // bu şekilde state değiştirilemez
+    setCount(count - 1);
+  }
+
+  console.log("component render edildi");
+
+  return (
+  
+   <div>
+
+
+      <div> {count} </div>
+      <div><button onClick={arttir} > Arttır</button></div>
+      <div><button onClick={azalt} > Azalt</button></div>
+
+
+  
+      { /* <div>
+          {show ? <div> {userInfo.name} {userInfo.password} </div> : <div> Kullanıcı Bilgisi Yok </div>}
+      </div > */}
+
+   
+
+      {/* <div>
+        <div>
+          {firstName}
+        </div>
+        <div>
+          <button onClick={handleChange}>İsmi Değiştir</button>
+        </div> */}
+
+        {/* {firstName} {lastName} */}
+      {/* </div> */}
+
    </div>
   )
 }
