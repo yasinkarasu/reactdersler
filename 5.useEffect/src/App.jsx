54ed05670b284e10354ea0021d4a56d304b90442
Yasin Karasu <yasin.karasu5734@gmail.com>
2025-07-31 12:06:27 +0300

    useeffect

--- 5.useEffect/src/App.jsx
+++ 5.useEffect/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstName, setFirstName] = useState("Yasin");
+  const [lastName, setLastName] = useState("Karasu");
+
+  useEffect(() => {
+    console.log("Her zaman çalışır");
+  }); // Her renderda çalışır
+
+  useEffect(() => {
+    console.log("ilk renderda çalışır");
+  }, []); // bir kez çalışır
+
+  useEffect(() => {
+    console.log(`firstName değiştiğinde çalışır: ${firstName}`);
+  }, [firstName]); // firstName değiştiğinde çalışır
+
+  useEffect(() => {
+    console.log(`lastName değiştiğinde çalışır: ${lastName}`);
+  }, [lastName]); // lastName değiştiğinde çalışır
 
   return (
-    <>
+    <div>
+      <div>First Name: {firstName}</div>
+      <div>Last Name: {lastName}</div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={() => setFirstName("Furkan")}>Adı Değiştir</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <button onClick={() => setLastName("Sarı")}>Soyadı Değiştir</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
