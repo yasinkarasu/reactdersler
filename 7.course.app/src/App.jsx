f0efca9fdb993545e65d1d20293c4c89ecd920fe
Yasin Karasu <yasin.karasu5734@gmail.com>
2025-07-31 17:26:20 +0300

    css

--- 7.course.app/src/App.jsx
+++ 7.course.app/src/App.jsx
@@ -5,24 +5,26 @@ import './App.css'
 import Header from './Header'
 import { courses } from './Data'
 import Course from './Component/Course'
+import './css/Course.css'
 function App() {
-
+ 
   return (
     <div>
 
-
       <div>
-        
           <Header />
 
+        <div className='course-main'>
+      
         {
           courses?.map((course) => (
             <Course key={course.id} course={course} />
           ))
         }
+
+         </div>
         
       </div> 
-
 
     </div>
 
--- 7.course.app/src/Component/Course.jsx
+++ 7.course.app/src/Component/Course.jsx
@@ -8,11 +8,11 @@ function Course( {course} ) {
     <div className='course'>
 
         <div>
-            <img src={image} width={250} height={250}/>
-            <h4>{title}</h4>
-            <h5>{description}</h5>
-            <h3>{price}</h3>
-            <a href={link}>satın almak için</a>
+            <img src={image} width={200} height={110}/>
+            <h4 className='course-title'>{title}</h4>
+            <h5 className='course-description'>{description}</h5>
+            <h3 className='course-price' >{price} ₺ </h3>
+            <a className='course-link' href={link}>satın almak için tıklayınız</a>
 
         </div>
 
