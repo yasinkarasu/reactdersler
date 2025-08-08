c7663e34d2b0305c593c543f35c6c87e8f34e009
Yasin Karasu <yasin.karasu5734@gmail.com>
2025-08-08 22:40:00 +0300

    Create-todo

--- 10.todo.app/src/App.jsx
+++ 10.todo.app/src/App.jsx
@@ -1,22 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import TodoCreate from './components/TodoCreate'
 import TodoList from './components/TodoList'
 
-
 function App() {
+  const [todos, setTodos] = useState([]);
 
+  const createTodo = (newTodo) => {
+    setTodos([...todos, newTodo]); // <-- düzeltildi
+  };
 
   return (
-
     <div className='App'>
-        <div style={{ width: '500px', display:'flex', flexDirection:'column', alignItems: 'center', justifyContent:'center'}}>
-          <TodoCreate /> 
-          <TodoList />
-        </div> 
+      <div className='main'>
+        <TodoCreate onCreateTodo={createTodo} />
+        <TodoList todos={todos} />
+      </div>
     </div>
   )
 }
+
 export default App
--- 10.todo.app/src/components/TodoCreate.jsx
+++ 10.todo.app/src/components/TodoCreate.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../App.css';
 
-function TodoCreate() {
+function TodoCreate({ onCreateTodo }) {
+  const [newTodo, setNewTodo] = useState('');
+
+  const createTodo = () => {
+    if (!newTodo.trim()) return;
+    const request = {
+      id: Math.floor(Math.random() * 1_000_000_000),
+      content: newTodo.trim(),
+    };
+    onCreateTodo(request);
+    setNewTodo('');
+  };
+
   return (
-    <div className='todo_create'>
-        <input className='todo-input' type="text" placeholder='Todo giriniz' />
-        <button className='todo-create-button'>Todo Oluştur</button>
+    <div className='todo-create'>
+      <input
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        className='todo-input'
+        type='text'
+        placeholder='Todo giriniz'
+      />
+      <button className='todo-create-button' onClick={createTodo}>
+        Todo Oluştur
+      </button>
     </div>
-  )
+  );
 }
 
-export default TodoCreate
+export default TodoCreate;
